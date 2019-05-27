cff4ad2894f9ed9bafd2d18f5efe06a3f64ad7f1
Greg1709 <44266426+Greg1709@users.noreply.github.com>
2019-05-27 23:54:36 +0200

    RollingRock implementation

--- BoulderDashA1/model/src/main/java/model/RollingRock.java
+++ BoulderDashA1/model/src/main/java/model/RollingRock.java
@@ -1,14 +1,23 @@
 package model;
 
 public class RollingRock extends SlidingBlock {
+    protected Player player;
+
 
     public RollingRock(Model model,int x, int y)
     {
         super(model,x,y,EntityType.ROLLINGROCK);
     }
 
-    public void moved()
+    public void moved() throws Exception
     {
 
+        if( getRelativeEntity(getPositionX()+2,getPositionY()) == null)
+        {
+            this.model.updateEntity(getPositionX()+1,getPositionY(),this);
+        }else if( getRelativeEntity(getPositionX()-2,getPositionY())==null)
+        {
+            this.model.updateEntity(getPositionX()-1,getPositionY(),this);
+        }
     }
 }
