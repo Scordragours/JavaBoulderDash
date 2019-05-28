adfd37dc85bcb693ad27f0c9a8bf90cef6313ce4
Greg1709 <44266426+Greg1709@users.noreply.github.com>
2019-05-28 22:27:09 +0200

    Add a JavaDoc commentary

--- BoulderDashA1/model/src/main/java/model/Entity.java
+++ BoulderDashA1/model/src/main/java/model/Entity.java
@@ -1,5 +1,9 @@
 package model;
-
+/**
+ * The class Controller
+ *
+ * @author DENEUVE GREGORY AND CANDAT ETIENNE
+ */
 
 public abstract class Entity {
 
@@ -7,7 +11,13 @@ public abstract class Entity {
     private int positionX;
     private int positionY;
     private EntityType type;
-
+    /**
+     * The constructor of entity
+     * @param model is a call from model class
+     * @param positionX it is the length position
+     * @param positionY it is the height position
+     * @param type it is the type of entity
+     */
     public Entity(Model model,int positionX, int positionY, EntityType type)
     {
         this.model = model;
@@ -15,12 +25,20 @@ public abstract class Entity {
         this.positionX = positionX;
         this.type = type;
     }
-
+    /**
+     *
+     * @return Returns an instance of type int which corresponds to the length of the entity
+     */
     public int getPositionX()
     {
         return this.positionX;
     }
 
+    /**
+     * Allows to add a position x to the entity
+     * @param x it is the length position
+     * @throws Exception if position values is negative
+     */
     public void setPositionX(int x) throws Exception
     {
         if(x<0)
@@ -31,11 +49,19 @@ public abstract class Entity {
         }
 
     }
-
+    /**
+     *
+     * @return Returns an instance of type int which corresponds to the height of the entity
+     */
     public int getPositionY()
     {
         return positionY;
     }
+    /**
+     * Allows to add update a position y to the entity
+     * @param y it is the height position
+     * @throws Exception if position values is negative
+     */
 
     public void setPositionY(int y) throws Exception
     {
@@ -48,12 +74,20 @@ public abstract class Entity {
 
     }
 
-
+    /**
+     *
+     * @return Returns an instance of type int which corresponds to the type of the entity
+     */
     public EntityType getType()
     {
             return type;
     }
-
+    /**
+     *
+     * @param x it is length position of entity
+     * @param y it is height position of entity
+     * @return relativePosition around the entity
+     */
     public Entity getRelativeEntity(int x, int y)
     {
         return this.model.getWorld()[positionY+y][positionX+x];
